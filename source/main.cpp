55b97584095ec09fb01912ab39466f83a2d8c763
Enrico Carraro <enrico.carraro@studenti.polito.it>
2021-01-22 12:26:48 +0100

    Add BENCHMARK option.

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,9 @@
 // READ_TYPE = 0 => C fscanf
 #define READ_TYPE 0
 
+// Set to 1 to time each phase of the algorithm.
+#define BENCHMARK 0
+
 using namespace std;
 
 int main(int argc, const char *argv[])
@@ -49,31 +52,39 @@ int main(int argc, const char *argv[])
         fclose(fp);
 #endif
 		gp.sortVectors();
+#if BENCHMARK
 		auto iend = std::chrono::steady_clock::now();
 		// gp.printGraph();
 		std::chrono::duration<double> ielapsed_seconds = iend - istart;
 		std::cout << "Init elapsed time: " << ielapsed_seconds.count() << "s\n";
 		auto start = std::chrono::steady_clock::now();
+#endif        
 		gp.buildDT();
+#if BENCHMARK
 		auto end = std::chrono::steady_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end - start;
 		std::cout << "DT elapsed time: " << elapsed_seconds.count() << "s\n";
 		auto sstart = std::chrono::steady_clock::now();
+#endif
 		gp.computeSubDTSize();
+#if BENCHMARK
 		auto send = std::chrono::steady_clock::now();
 		std::chrono::duration<double> selapsed_seconds = send - sstart;
 		std::cout << "subgraph size elapsed time: " << selapsed_seconds.count() << "s\n";
 		auto ppstart = std::chrono::steady_clock::now();
+#endif
 		gp.computePrePostOrder();
 		gp.computeLabels();
+#if BENCHMARK
 		auto ppend = std::chrono::steady_clock::now();
 		std::chrono::duration<double> ppelapsed_seconds = ppend - ppstart;
 		std::cout << "pre post elapsed time: " << ppelapsed_seconds.count() << "s\n";
-		//gp.printGraph();
-		auto itotalend = std::chrono::steady_clock::now();
+        auto itotalend = std::chrono::steady_clock::now();
 		std::chrono::duration<double> pptotal = itotalend - itotal;
 		cout << "total time taken:" << pptotal.count() << endl;
-		//gp.printNodesStatus();
+#endif
+		//gp.printGraph();
+        gp.printNodesStatus();
 	}
 	else if (strcmp(argv[2], "sequential") == 0)
 	{
@@ -86,16 +97,20 @@ int main(int argc, const char *argv[])
         fclose(fp);
 #endif
 		gs.sortVectors();
+#if BENCHMARK
 		auto iend = std::chrono::steady_clock::now();
 		std::chrono::duration<double> ielapsed_seconds = iend - istart;
 		std::cout << "Init elapsed time: " << ielapsed_seconds.count() << "s\n";
 		auto start = std::chrono::steady_clock::now();
+#endif
 		gs.sequentialDFS();
+#if BENCHMARK
 		auto end = std::chrono::steady_clock::now();
 		std::chrono::duration<double> elapsed_seconds = end - start;
 		std::cout << "Sequential elapsed time: " << elapsed_seconds.count() << "s\n";
+#endif
 		//gs.printGraph();
-		//  gs.printNodesStatus();
+		gs.printNodesStatus();
 	}
 	return 0;
 }
